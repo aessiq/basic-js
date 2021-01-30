b381b5abe13011fac3f769078a637b855cea0384
aessiq <noriod@yandex.ru>
2021-01-31 01:39:14 +0300

    feat: solve Carbin dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,7 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (+sampleActivity != "number") return false;
+  return Math.ceil((Math.log(+sampleActivity/MODERN_ACTIVITY))/(0.693/HALF_LIFE_PERIOD));
 };
