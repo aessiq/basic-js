cbcdbd66095349d54ee16bb36e53ee075c0db150
aessiq <noriod@yandex.ru>
2021-02-07 00:28:16 +0300

    feat: solve Simple Chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
+let result = "";
+
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push("( " + String(value) + " )");
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!position) {
+      this.chain = [];
+      throw new Error();
+    };
+    this.chain.splice(position-1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    result = this.chain.join("~~");
+    this.chain = [];
+    return result;
   }
 };
 
