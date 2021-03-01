b68bad0bcb40537da5a288f50ab5897fc4611833
aessiq <noriod@yandex.ru>
2021-03-01 17:26:29 +0300

    feat: add encryption

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,56 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
+  constructor(isDirect) {
+    if (isDirect == false) {
+      this.isDirect = false;
+    } else {
+      this.isDirect = true;
+    }
+  }
+
+  encrypt(message, key) {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let m = message.toLowerCase().split('');
+    let k = [];
+    let result = [];
+
+    if (message.length != key.length || k.length == 0) {
+      let i = 0;
+      let j = 0;
+      while (m.length != k.length) {
+  
+        if (alphabet.indexOf(m[i]) > -1) {
+          k.push(key[j].toLowerCase());
+          i++;
+          j++;
+          if (j == key.length) {
+            j = 0;
+          }
+        } else {
+          k.push(m[i]);
+          i++;
+        }
+      }
+    }
+
+    for (let i = 0; i < m.length; i++) {
+      if (alphabet.indexOf(m[i]) > -1) {
+      result.push(alphabet[(alphabet.indexOf(m[i]) + alphabet.indexOf(k[i])) % 26])
+      } else {
+        result.push(m[i]);
+      }
+    }
+    
+    if (this.isDirect) {
+      return result.join('').toUpperCase();
+    } else {
+      return result.reverse().join('').toUpperCase();
+    }
+  }  
+
   decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    
   }
 }
 
